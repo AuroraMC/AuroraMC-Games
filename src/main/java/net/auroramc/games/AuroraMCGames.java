d6ce1c8c77c7ad34cce09ed0e252a196d66dcd93
Block2Block <accounts@block2block.me>
2021-09-10 20:56:11 +0100

    Initial Commit

---
+++ src/main/java/net/auroramc/games/AuroraMCGames.java
@@ -0,0 +1,12 @@
+package net.auroramc.games;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AuroraMCGames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
