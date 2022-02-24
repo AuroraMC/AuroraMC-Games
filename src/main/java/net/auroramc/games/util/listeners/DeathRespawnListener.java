138ce876c655b242b70b002d5ea4452432f7dee8
Block2Block <accounts@block2block.me>
2022-02-24 13:10:59 +0000

    Take into account lastest hits

--- src/main/java/net/auroramc/games/util/listeners/DeathRespawnListener.java
+++ src/main/java/net/auroramc/games/util/listeners/DeathRespawnListener.java
@@ -7,10 +7,12 @@ package net.auroramc.games.util.listeners;
 import net.auroramc.core.api.AuroraMCAPI;
 import net.auroramc.core.api.cosmetics.Cosmetic;
 import net.auroramc.core.api.cosmetics.KillMessage;
+import net.auroramc.core.api.players.AuroraMCPlayer;
 import net.auroramc.engine.api.EngineAPI;
 import net.auroramc.engine.api.players.AuroraMCGamePlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
@@ -23,6 +25,8 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.json.JSONObject;
+
+import java.util.Map;
 
 public class DeathRespawnListener implements Listener {
 
@@ -116,10 +120,69 @@ public class DeathRespawnListener implements Listener {
                         killReason = KillMessage.KillReason.ENTITY;
                     }
                 } else {
-                    killReason = KillMessage.KillReason.UNKNOWN;
+                    switch (e.getCause()) {
+                        case FALL: {
+                            killReason = KillMessage.KillReason.FALL;
+                            if (player.getLastHitBy() != null) {
+                                killer = player.getLastHitBy();
+                            }
+                            break;
+                        }
+                        case VOID: {
+                            killReason = KillMessage.KillReason.VOID;
+                            if (player.getLastHitBy() != null) {
+                                killer = player.getLastHitBy();
+                            }
+                            break;
+                        }
+                        case LAVA: {
+                            killReason = KillMessage.KillReason.LAVA;
+                            if (player.getLastHitBy() != null) {
+                                killer = player.getLastHitBy();
+                            }
+                            break;
+                        }
+                        case FIRE_TICK: {
+                            killReason = KillMessage.KillReason.FIRE;
+                            if (player.getLastHitBy() != null) {
+                                killer = player.getLastHitBy();
+                            }
+                            break;
+                        }
+                        case DROWNING: {
+                            killReason = KillMessage.KillReason.DROWNING;
+                            if (player.getLastHitBy() != null) {
+                                killer = player.getLastHitBy();
+                            }
+                            break;
+                        }
+                        default: {
+                            killReason = KillMessage.KillReason.UNKNOWN;
+                        }
+                    }
                 }
 
                 String finalMessage = killMessage.onKill(player, killer, entity, killReason);
+
+                if (killer != null) {
+                    killer.getRewards().addXp("Kills", 25);
+                    killer.getStats().incrementStatistic(EngineAPI.getActiveGameInfo().getId(), "kills", 1, true);
+
+                    //If there is a killer, give out assists.
+                    for (Map.Entry<AuroraMCGamePlayer, Long> entry : player.getLatestHits().entrySet()) {
+                        if (System.currentTimeMillis() - entry.getValue() < 60000 && entry.getKey().getId() != killer.getId()) {
+                            entry.getKey().getRewards().addXp("Assists", 10);
+                            entry.getKey().getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Kill", "You got an assist on player **" + player.getPlayer().getName() + "**!"));
+                            entry.getKey().getPlayer().playSound(entry.getKey().getPlayer().getLocation(), Sound.ARROW_HIT, 100, 1);
+                        }
+                    }
+                }
+
+                player.getStats().incrementStatistic(EngineAPI.getActiveGameInfo().getId(), "deaths", 1, true);
+
+                player.setLastHitAt(-1);
+                player.setLastHitBy(null);
+                player.getLatestHits().clear();
 
                 for (Player player2 : Bukkit.getOnlinePlayers()) {
                     player2.hidePlayer(player.getPlayer());
@@ -140,6 +203,14 @@ public class DeathRespawnListener implements Listener {
                     }
                 }.runTaskLater(AuroraMCAPI.getCore(), timeout);
 
+            } else if (e instanceof EntityDamageByEntityEvent) {
+                if (e.getFinalDamage() > 0 && ((EntityDamageByEntityEvent) e).getDamager() instanceof Player) {
+                    AuroraMCGamePlayer player1 = (AuroraMCGamePlayer) AuroraMCAPI.getPlayer((Player) ((EntityDamageByEntityEvent) e).getDamager());
+                    long time = System.currentTimeMillis();
+                    player.setLastHitBy(player1);
+                    player.setLastHitAt(time);
+                    player.getLatestHits().put(player1, time);
+                }
             }
         }
     }
