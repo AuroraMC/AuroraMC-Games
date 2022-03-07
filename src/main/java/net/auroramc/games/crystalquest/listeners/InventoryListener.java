7dde4373e7694ec2f0baa6fa492946e1ac45e75e
Block2Block <accounts@block2block.me>
2022-03-07 19:33:45 +0000

    Stop players dropping kit items

--- src/main/java/net/auroramc/games/crystalquest/listeners/InventoryListener.java
+++ src/main/java/net/auroramc/games/crystalquest/listeners/InventoryListener.java
@@ -7,10 +7,12 @@ package net.auroramc.games.crystalquest.listeners;
 import net.auroramc.core.api.AuroraMCAPI;
 import net.auroramc.engine.api.EngineAPI;
 import net.auroramc.engine.api.server.ServerState;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -29,6 +31,14 @@ public class InventoryListener implements Listener {
                     }
                 }.runTaskLater(AuroraMCAPI.getCore(), 3);
             }
+        }
+    }
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent e) {
+        if (e.getItemDrop().getItemStack().getType().name().endsWith("_SWORD") || e.getItemDrop().getItemStack().getType().name().endsWith("_PICKAXE") || e.getItemDrop().getItemStack().getType().name().endsWith("_AXE") || e.getItemDrop().getItemStack().getType() == Material.COMPASS || e.getItemDrop().getItemStack().getType().name().endsWith("_HELMET") || e.getItemDrop().getItemStack().getType().name().endsWith("_CHESTPLATE") || e.getItemDrop().getItemStack().getType().name().endsWith("_LEGGINGS") || e.getItemDrop().getItemStack().getType().name().endsWith("_BOOTS")) {
+            e.setCancelled(true);
+            e.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Game", "You cannot drop this item!"));
         }
     }
 
