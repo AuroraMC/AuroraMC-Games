d49c4318fd89e4f49d8f9a097d8f8517b9cd9e29
Block2Block <accounts@block2block.me>
2022-04-07 18:05:15 +0100

    Change Hot Potato Name

--- src/main/java/net/auroramc/games/hotpotato/HotPotatoInfo.java
+++ src/main/java/net/auroramc/games/hotpotato/HotPotatoInfo.java
@@ -9,6 +9,6 @@ import net.auroramc.engine.api.games.GameInfo;
 public class HotPotatoInfo extends GameInfo {
 
     public HotPotatoInfo() {
-        super(101, "HotPotato", HotPotato.class, "A game description", "HOTPOTATO", false, "v0.0.1-ALPHA");
+        super(101, "Hot Potato", HotPotato.class, "A game description", "HOT_POTATO", false, "v0.0.1-ALPHA");
     }
 }
