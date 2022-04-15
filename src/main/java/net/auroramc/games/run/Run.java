2590420d5eae37555505cf0dc20b641cd9e26012
Block2Block <accounts@block2block.me>
2022-04-15 12:28:13 +0300

    Another fix

--- src/main/java/net/auroramc/games/run/Run.java
+++ src/main/java/net/auroramc/games/run/Run.java
@@ -148,6 +148,81 @@ public class Run extends Game {
                     }
                 }.runTaskLater(AuroraMCAPI.getCore(), 20);
             }
+            int wholeX = (int) location.getX();
+            int wholeZ = (int) location.getZ();
+            double decX = Math.abs(location.getX() - wholeX);
+            double decZ = Math.abs(location.getZ() - wholeZ);
+            Location x = null;
+            Location z = null;
+            if (decX <= 0.31) {
+                x = location.clone();
+                if (location.getX() > 0) {
+                    x.setX(x.getX() - 1);
+                } else {
+                    x.setX(x.getX() + 1);
+                }
+            } else if (decX >= 0.71) {
+                x = location.clone();
+                if (location.getX() > 0) {
+                    x.setX(x.getX() + 1);
+                } else {
+                    x.setX(x.getX() - 1);
+                }
+            }
+            if (x != null && x.getBlock().getType() != Material.AIR && (x.getBlock().getType() != Material.STAINED_CLAY || x.getBlock().getData() != 14) && !x.getBlock().isLiquid()) {
+                x.getBlock().setType(Material.STAINED_CLAY);
+                x.getBlock().setData((byte)14);
+                Location finX = x;
+                new BukkitRunnable(){
+                    @Override
+                    public void run() {
+                        finX.getBlock().setType(Material.AIR);
+                    }
+                }.runTaskLater(AuroraMCAPI.getCore(), 20);
+            }
+
+            if (decZ <= 0.31) {
+                z = location.clone();
+                if (location.getZ() > 0) {
+                    z.setZ(z.getZ() - 1);
+                } else {
+                    z.setZ(z.getZ() + 1);
+                }
+            } else if (decZ >= 0.71) {
+                z = location.clone();
+                if (location.getZ() > 0) {
+                    z.setZ(z.getZ() + 1);
+                } else {
+                    z.setZ(z.getZ() - 1);
+                }
+            }
+            if (z != null) {
+                if (z.getBlock().getType() != Material.AIR && (z.getBlock().getType() != Material.STAINED_CLAY || z.getBlock().getData() != 14) && !z.getBlock().isLiquid()) {
+                    z.getBlock().setType(Material.STAINED_CLAY);
+                    z.getBlock().setData((byte)14);
+                    Location finZ = z;
+                    new BukkitRunnable(){
+                        @Override
+                        public void run() {
+                            finZ.getBlock().setType(Material.AIR);
+                        }
+                    }.runTaskLater(AuroraMCAPI.getCore(), 20);
+                }
+                if (x != null) {
+                    Location loc2 = x.clone();
+                    loc2.setZ(z.getZ());
+                    if (loc2.getBlock().getType() != Material.AIR && (loc2.getBlock().getType() != Material.STAINED_CLAY || loc2.getBlock().getData() != 14) && !loc2.getBlock().isLiquid()) {
+                        loc2.getBlock().setType(Material.STAINED_CLAY);
+                        loc2.getBlock().setData((byte)14);
+                        new BukkitRunnable(){
+                            @Override
+                            public void run() {
+                                loc2.getBlock().setType(Material.AIR);
+                            }
+                        }.runTaskLater(AuroraMCAPI.getCore(), 20);
+                    }
+                }
+            }
         }
     }
 
