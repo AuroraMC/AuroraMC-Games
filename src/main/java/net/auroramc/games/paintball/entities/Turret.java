2b8819242f5fdc0938f5a963a83f4bbda0906467
Block2Block <accounts@block2block.me>
2022-04-19 23:21:04 +0300

    Change turrets

--- src/main/java/net/auroramc/games/paintball/entities/Turret.java
+++ src/main/java/net/auroramc/games/paintball/entities/Turret.java
@@ -4,44 +4,97 @@
 
 package net.auroramc.games.paintball.entities;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import net.auroramc.core.api.AuroraMCAPI;
 import net.auroramc.core.api.players.AuroraMCPlayer;
+import net.auroramc.core.api.utils.gui.GUIItem;
 import net.auroramc.engine.api.EngineAPI;
 import net.auroramc.engine.api.players.AuroraMCGamePlayer;
-import net.minecraft.server.v1_8_R3.NBTTagCompound;
+import net.auroramc.games.paintball.teams.PBBlue;
+import org.bukkit.Color;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftSnowman;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Snowball;
-import org.bukkit.entity.Snowman;
+import org.bukkit.Material;
+import org.bukkit.entity.*;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
+import java.lang.reflect.Field;
+import java.util.Base64;
 import java.util.List;
+import java.util.UUID;
 
 public class Turret {
 
+    private static final ItemStack RED_HEAD;
+    private static final ItemStack BLUE_HEAD;
+
+    static {
+        init: {
+            GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+            profile.getProperties().put("textures", new Property("textures", Base64.getEncoder().encodeToString("{textures:{SKIN:{url:\"http://textures.minecraft.net/texture/4b4b148778377b7ce25c8dfb346a5d9429dfff72f12f67d0886826a1f7266a0\"}}}".getBytes())));
+
+            RED_HEAD = new ItemStack(Material.SKULL_ITEM, 1);
+            RED_HEAD.setDurability((short)3);
+            SkullMeta meta = (SkullMeta) RED_HEAD.getItemMeta();
+            Field field;
+            try {
+                field = meta.getClass().getDeclaredField("profile");
+            } catch (NoSuchFieldException | SecurityException e) {
+                e.printStackTrace();
+                BLUE_HEAD = null;
+                break init;
+            }
+            field.setAccessible(true);
+            try {
+                field.set(meta, profile);
+            } catch (IllegalAccessException | IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+            RED_HEAD.setItemMeta(meta);
+
+            profile = new GameProfile(UUID.randomUUID(), null);
+            profile.getProperties().put("textures", new Property("textures", Base64.getEncoder().encodeToString("{textures:{SKIN:{url:\"http://textures.minecraft.net/texture/e0b0b3c047ca73a3f40597efd4e87cfbe2124a3c3a67b8665f358e66eb4e91c5\"}}}".getBytes())));
+
+            BLUE_HEAD = new ItemStack(Material.SKULL_ITEM, 1);
+            BLUE_HEAD.setDurability((short)3);
+            meta = (SkullMeta) BLUE_HEAD.getItemMeta();
+            try {
+                field = meta.getClass().getDeclaredField("profile");
+            } catch (NoSuchFieldException | SecurityException e) {
+                e.printStackTrace();
+                break init;
+            }
+            field.setAccessible(true);
+            try {
+                field.set(meta, profile);
+            } catch (IllegalAccessException | IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+            BLUE_HEAD.setItemMeta(meta);
+        }
+    }
+
     private final Location location;
-    private final Snowman snowman;
-    private BukkitTask task;
-    private AuroraMCPlayer owner;
+    private final ArmorStand armorStand;
+    private final BukkitTask task;
+    private final AuroraMCPlayer owner;
 
     public Turret(AuroraMCPlayer owner, Location location) {
         this.location = location;
-        this.snowman = location.getWorld().spawn(location, Snowman.class);
+        this.armorStand = location.getWorld().spawn(location, ArmorStand.class);
+        armorStand.setBasePlate(false);
+        armorStand.setArms(true);
+        armorStand.setHelmet(((owner.getTeam() instanceof PBBlue)?BLUE_HEAD:RED_HEAD));
+        Color data = ((owner.getTeam() instanceof PBBlue)?Color.BLUE:Color.RED);
+        armorStand.setChestplate(new GUIItem(Material.LEATHER_CHESTPLATE, null, 1, null, (short)0, false, data).getItem());
+        armorStand.setLeggings(new GUIItem(Material.LEATHER_LEGGINGS, null, 1, null, (short)0, false, data).getItem());
+        armorStand.setBoots(new GUIItem(Material.LEATHER_BOOTS, null, 1, null, (short)0, false, data).getItem());
+        armorStand.setItemInHand(new ItemStack(((owner.getTeam() instanceof PBBlue)?Material.DIAMOND_BARDING:Material.GOLD_BARDING)));
         this.owner = owner;
-        CraftEntity craftEntity = ((CraftEntity)snowman);
-        NBTTagCompound tag = craftEntity.getHandle().getNBTTag();
-        if (tag == null) {
-            tag = new NBTTagCompound();
-        }
-        craftEntity.getHandle().c(tag);
-        tag.setInt("NoAI", 1);
-        tag.setInt("Invulnerable", 1);
-        craftEntity.getHandle().f(tag);
 
         task = new BukkitRunnable(){
 
@@ -49,7 +102,7 @@ public class Turret {
 
             @Override
             public void run() {
-                List<Entity> closeEntities = snowman.getNearbyEntities(12, 12, 12);
+                List<Entity> closeEntities = armorStand.getNearbyEntities(12, 12, 12);
                 if (closeEntities.size() > 0) {
                     Player closest = null;
                     for (Entity entity : closeEntities) {
@@ -58,7 +111,7 @@ public class Turret {
                             if (!player.isSpectator() && !player.getTeam().equals(owner.getTeam())) {
                                 closest = (Player) entity;
                             }
-                        } else if (entity instanceof Player && closest.getLocation().distanceSquared(snowman.getLocation()) > entity.getLocation().distanceSquared(snowman.getLocation())) {
+                        } else if (entity instanceof Player && closest.getLocation().distanceSquared(armorStand.getLocation()) > entity.getLocation().distanceSquared(armorStand.getLocation())) {
                             AuroraMCGamePlayer player = (AuroraMCGamePlayer) AuroraMCAPI.getPlayer((Player) entity);
                             if (!player.isSpectator() && !player.getTeam().equals(owner.getTeam())) {
                                 closest = (Player) entity;
@@ -67,16 +120,15 @@ public class Turret {
                     }
                     if (closest != null) {
                         Vector vector = closest.getLocation().toVector();
-                        vector.subtract(snowman.getLocation().toVector());
-                        Location loc = snowman.getLocation().setDirection(vector);
-                        ((CraftSnowman)snowman).getHandle().setLocation(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
+                        vector.subtract(armorStand.getLocation().toVector());
+                        armorStand.teleport(armorStand.getLocation().setDirection(vector));
                         vector.normalize();
-                        snowman.launchProjectile(Snowball.class, vector);
+                        armorStand.launchProjectile(Snowball.class, vector);
                     }
                 }
                 lifetime++;
                 if (lifetime >= 30) {
-                    snowman.remove();
+                    armorStand.remove();
                     owner.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Game", "One of your turrets has expired!"));
                     this.cancel();
                 }
@@ -85,8 +137,8 @@ public class Turret {
     }
 
 
-    public Snowman getSnowman() {
-        return snowman;
+    public ArmorStand getArmorStand() {
+        return armorStand;
     }
 
     public Location getLocation() {
