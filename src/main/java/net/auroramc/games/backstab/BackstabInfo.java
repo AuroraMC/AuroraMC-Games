7a2e2b0ff763269a3fae47e6de0fecc0aa96243b
Block2Block <accounts@block2block.me>
2023-07-04 16:47:08 +0100

    Fix backstab

--- src/main/java/net/auroramc/games/backstab/BackstabInfo.java
+++ src/main/java/net/auroramc/games/backstab/BackstabInfo.java
@@ -24,6 +24,6 @@ public class BackstabInfo extends GameInfo {
 
     public BackstabInfo() {
         super(2, "Backstab", Backstab.class, "Maneuver your way through enemy territory and collect their crystals " +
-                "to allow you to eliminate them. Upgrade armour and enchants to give your team an advantage, but remember: §bProtect your crystals at all time!", "CRYSTAL_QUEST", false, "v0.1.3-ALPHA", variations);
+                "to allow you to eliminate them. Upgrade armour and enchants to give your team an advantage, but remember: §bProtect your crystals at all time!", "CRYSTAL_QUEST", false, variations);
     }
 }
