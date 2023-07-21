9439d6f3d570dd47394f9df28a72b84e486a1b11
Block2Block <accounts@block2block.me>
2023-07-21 16:38:14 +0100

    Fix Infinite Ammo

--- src/main/java/net/auroramc/games/backstab/BackstabInfo.java
+++ src/main/java/net/auroramc/games/backstab/BackstabInfo.java
@@ -9,6 +9,7 @@ package net.auroramc.games.backstab;
 import net.auroramc.engine.api.games.GameInfo;
 import net.auroramc.engine.api.games.GameVariationInfo;
 import net.auroramc.games.crystalquest.CrystalQuest;
+import org.bukkit.Material;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +25,6 @@ public class BackstabInfo extends GameInfo {
 
     public BackstabInfo() {
         super(2, "Backstab", Backstab.class, "Maneuver your way through enemy territory and collect their crystals " +
-                "to allow you to eliminate them. Upgrade armour and enchants to give your team an advantage, but remember: §bProtect your crystals at all time!", "CRYSTAL_QUEST", false, variations);
+                "to allow you to eliminate them. Upgrade armour and enchants to give your team an advantage, but remember: §bProtect your crystals at all time!", "CRYSTAL_QUEST", false, variations, Material.IRON_SWORD, (short)0);
     }
 }
